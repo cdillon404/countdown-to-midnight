775bbff89f36f68d227f5d67fcef0b6c3c5ed200
cdillon404 <chavin@gmail.com>
2020-11-20 16:20:43 -0700

    commit tree e43800ba94756e7ed6ac0a05d4eb8c4062bd3b83

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown (x)
+    while x > 0
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+       return "HAPPY NEW YEAR!"
 end
+
+countdown(12)
+
+
+def countdown_with_sleep(x)
+    while x > 0
+        sleep 1
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+end
+
+countdown_with_sleep(8)
